55309099c43a1b178c8463db7539bda132989461
YulDmi <barsetca@rambler.ru>
2020-04-29 19:13:29 +0300

    движение до заданной точки

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -1,37 +1,15 @@
 package ru.geekbrains;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img, img2;
+
+import ru.geekbrains.screen.MenuScreen;
+
+public class StarGame extends Game {
+
 
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img2 = new Texture("69.jpg");
-		img = new Texture("badlogic.jpg");
-
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0.5f, 0, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img2, 0, 0, 650, 500);
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-		img2.dispose();
+	public void create() {
+		setScreen(new MenuScreen());
 	}
 }
