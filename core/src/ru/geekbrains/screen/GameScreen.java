2e637309e48b97600206e20d36a570b94b54c3f8
YulDmi <barsetca@rambler.ru>
2020-05-06 23:51:33 +0300

    два экрана

--- core/src/ru/geekbrains/screen/GameScreen.java
+++ core/src/ru/geekbrains/screen/GameScreen.java
@@ -3,43 +3,29 @@ package ru.geekbrains.screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
-import ru.geekbrains.StarGame;
 import ru.geekbrains.base.BaseScreen;
 import ru.geekbrains.math.Rect;
 import ru.geekbrains.sprite.Background;
 import ru.geekbrains.sprite.ExitButton;
 import ru.geekbrains.sprite.Logo;
-import ru.geekbrains.sprite.PlayButton;
 
-public class MenuScreen extends BaseScreen {
-    private StarGame starGame;
+public class GameScreen extends BaseScreen {
     private Texture bg;
+    private Texture img;
     private Texture eb;
-    private Texture pb;
-    private Background background;
     private ExitButton exitButton;
-    private PlayButton playButton;
-
-    public MenuScreen(StarGame starGame) {
-        this.starGame = starGame;
-    }
+    private Background background;
+    private Logo logo;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("69.jpg");
+        img = new Texture("badlogic.jpg");
         eb = new Texture("exit.png");
-        pb = new Texture("play.png");
         background = new Background(bg);
+        logo = new Logo(img);
         exitButton = new ExitButton(eb);
-        playButton = new PlayButton(pb);
-    }
-
-    @Override
-    public void resize(Rect worldBounds) {
-        background.resize(worldBounds);
-        exitButton.resize(worldBounds);
-        playButton.resize(worldBounds);
     }
 
     @Override
@@ -47,32 +33,37 @@ public class MenuScreen extends BaseScreen {
         super.render(delta);
         batch.begin();
         background.draw(batch);
+        logo.draw(batch);
         exitButton.draw(batch);
-        playButton.draw(batch);
         batch.end();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        logo.resize(worldBounds);
+        exitButton.resize(worldBounds);
     }
 
     @Override
     public void dispose() {
         bg.dispose();
+        img.dispose();
         eb.dispose();
-        pb.dispose();
         super.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        exitButton.touchDown(touch, pointer, button);
-        playButton.touchDown(touch, pointer, button);
+        logo.touchDown(touch, pointer, button);
+        exitButton.touchDown(touch,pointer,button);
         return false;
     }
 
     @Override
     public boolean touchUp(Vector2 touch, int pointer, int button) {
-        exitButton.touchUp(touch, pointer, button);
-        if (playButton.touchUp(touch, pointer, button)){
-            starGame.setScreen(new GameScreen());
-        }
+        exitButton.touchUp(touch,pointer,button);
         return false;
     }
 }
